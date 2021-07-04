7d660db8c05394198db20993111fe85f7ff206f1
sabriyan_fabelio <sabriyan@fabelio.com>
2021-07-04 09:53:32 +0700

    fixing get consultation

--- app/Repositories/ConsultationRepository.php
+++ app/Repositories/ConsultationRepository.php
@@ -60,8 +60,8 @@ class ConsultationRepository
             $model = $model->where('consultationId', '=', $params['consultation_id']);
         }
         $model = $model->limit($params['limit'] ?? 10);
-        $model = $model->orderBy('consultationId');
         if(isset($params['start_after'])) {
+            $model = $model->orderBy('consultationId');
             $model = $model->startAfter([$params['start_after'] ?? '']);
         }
         if(isset($params['end_at'])) {

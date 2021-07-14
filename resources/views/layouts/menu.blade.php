59c5e94327ced5f611a914dd64472273eb36f805
Hendrik <drikdoank@gmail.com>
2021-07-14 18:29:21 +0700

    dashboard

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -5,3 +5,119 @@
         <p>Home</p>
     </a>
 </li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-table"></i>
+      <p>
+        Projek
+        <i class="fas fa-angle-left right"></i>
+      </p>
+    </a>
+    <ul class="nav nav-treeview" style="display: none;">
+      <li class="nav-item">
+        <a href="pages/tables/simple.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Simple Tables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/data.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>DataTables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/jsgrid.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>jsGrid</p>
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-table"></i>
+      <p>
+        Master Data
+        <i class="fas fa-angle-left right"></i>
+      </p>
+    </a>
+    <ul class="nav nav-treeview" style="display: none;">
+      <li class="nav-item">
+        <a href="pages/tables/simple.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Simple Tables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/data.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>DataTables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/jsgrid.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>jsGrid</p>
+        </a>
+      </li>
+    </ul>
+  </li>  
+  <li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-table"></i>
+      <p>
+        Payment
+        <i class="fas fa-angle-left right"></i>
+      </p>
+    </a>
+    <ul class="nav nav-treeview" style="display: none;">
+      <li class="nav-item">
+        <a href="pages/tables/simple.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Simple Tables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/data.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>DataTables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/jsgrid.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>jsGrid</p>
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-table"></i>
+      <p>
+        File Managemen
+        <i class="fas fa-angle-left right"></i>
+      </p>
+    </a>
+    <ul class="nav nav-treeview" style="display: none;">
+      <li class="nav-item">
+        <a href="pages/tables/simple.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Simple Tables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/data.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>DataTables</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/tables/jsgrid.html" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>jsGrid</p>
+        </a>
+      </li>
+    </ul>
+  </li>

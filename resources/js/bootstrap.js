a5e0d21e82bc6417268d60d22044f84d7f45be50
Hendrik <drikdoank@gmail.com>
2021-07-25 21:34:06 +0700

    data table

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,19 +13,24 @@ try {
     require('bootstrap');
     window.JSZip = require("jszip");
     //require( "pdfmake" );
-    require( 'datatables.net-bs4' );
-    require( 'datatables.net-buttons-bs4' );
-    require( 'datatables.net-buttons/js/buttons.colVis.js' );
-    require( 'datatables.net-buttons/js/buttons.flash.js' );
-    require( 'datatables.net-buttons/js/buttons.html5.js' );
-    require( 'datatables.net-buttons/js/buttons.print.js' );
-    require( 'datatables.net-colreorder-bs4' );
-    require( 'datatables.net-fixedcolumns-bs4' );
-    require( 'datatables.net-responsive-bs4' );
-    require( 'datatables.net-rowreorder-bs4' );
-    require( 'datatables.net-scroller-bs4' );
-    require( 'datatables.net-keytable' );
-    require( 'datatables.net-rowgroup' );
+    require( 'datatables.net-bs4');
+    require( 'datatables.net-buttons-bs4');
+    require( 'datatables.net-buttons/js/buttons.colVis.js');
+    require( 'datatables.net-buttons/js/buttons.flash.js');
+    require( 'datatables.net-buttons/js/buttons.html5.js');
+    require( 'datatables.net-buttons/js/buttons.print.js');
+    require( 'datatables.net-colreorder-bs4');
+    require( 'datatables.net-fixedcolumns-bs4');
+    require( 'datatables.net-responsive-bs4');
+    require( 'datatables.net-rowreorder-bs4');
+    require( 'datatables.net-scroller-bs4');
+    require( 'datatables.net-keytable-bs4');
+    require( 'datatables.net-rowgroup-bs4');
+    require( 'datatables.net-responsive-bs4');
+    require( 'datatables.net-searchpanes-bs4');
+    require( 'datatables.net-select-bs4');
+    require( 'daterangepicker');
+
 } catch (e) {
 }
 
--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -12,13 +12,7 @@
                     <div class="alert alert-danger">
                         {{ session('gagal') }}
                     </div>
-                @endif   
-                <div class="mt-2">
-                    <div class="form-group">
-                        From: <input type="date" name="date_start" id="date_start" class="datepicker" />
-                        To: <input type="date" name="date_end" id="date_end" class="datepicker" />
-                    </div>
-                </div>
+                @endif
                 <div class="box">
                     <div class="box-body">
                         <table class="table table-bordered table-striped" id="projectrange">
@@ -71,11 +65,6 @@
     @parent
     <script type="text/javascript">
         $(function() {
-            vStartDate = "{{ $start_date }}";
-            vEndDate = "{{ $end_date }}";
-            document.getElementById("date_start").valueAsDate = vStartDate ? new Date(vStartDate) : new Date();
-            document.getElementById("date_end").valueAsDate = vEndDate ? new Date(vEndDate) : new Date();
-
             var dTable = $('#projectrange').dataTable({
                 'paging': true,
                 'lengthChange': true,
@@ -92,26 +81,6 @@
                     }
                 }, ]
             });
-
-            $("#date_start").keyup(function() {
-                searchByDate();
-            });
-            $("#date_start").change(function() {
-                searchByDate();
-            });
-            $("#date_end").keyup(function() {
-                searchByDate();
-            });
-            $("#date_end").change(function() {
-                searchByDate();
-            });
-
-            function searchByDate() {
-                var dateStart = $('#date_start').val();
-                var dateEnd = $('#date_end').val();
-                var url = 'project?date_start=' + dateStart + '&date_end=' + dateEnd;
-                window.open(url, "_self")
-            }
         });
     </script>
 @endsection

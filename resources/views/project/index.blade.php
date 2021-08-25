724b033c69f32d3ca214edab7eedcfc1943fc879
Hendrik <drikdoank@gmail.com>
2021-08-25 12:20:54 +0700

    login regis integrate

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -20,14 +20,14 @@
                                 <tr>
                                     <th>No.</th>
                                     <th>Order Number</th>
-                                    <th>Room Number</th>
-                                    <th>Consultation Number</th>
                                     <th>Customer Name</th>
                                     <th>Applicator Name</th>
                                     <th>Description</th>
                                     <th>Address</th>
                                     <th>Status</th>
                                     <th>Date</th>
+                                    <th>Room Number</th>
+                                    <th>Consultation Number</th>
                                     <th>Actions</th>
                                 </tr>
                             </thead>
@@ -36,14 +36,14 @@
                                     <tr>
                                         <td>{{ ++$key }}</td>
                                         <td>{{ $project->order_number }}</td>
-                                        <td>{{ $project->room_id }}</td>
-                                        <td>{{ $project->consultation_id }}</td>
                                         <td>{{ $project->customer_name }}</td>
                                         <td>{{ $project->vendor_name }}</td>
                                         <td>{{ $project->description }}</td>
                                         <td>{{ $project->street }}</td>
                                         <td>{{ $project->status }}</td>
                                         <td>{{ $project->created_at->format('Y-m-d') }}</td>
+                                        <td>{{ $project->room_id }}</td>
+                                        <td>{{ $project->consultation_id }}</td>
                                         <td width="15%">
                                             <a href="{{ route('proyek.edit', ['proyek' => $project->id]) }}"
                                                 class="btn btn-sm btn-primary"> <i class="glyphicon glyphicon-edit"></i>

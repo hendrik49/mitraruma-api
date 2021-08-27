ffe75b3a60a8b2070e89eb10f1634da6d9a8a1d6
Hendrik <drikdoank@gmail.com>
2021-08-27 09:33:30 +0700

    log middleware

--- app/Http/Middleware/LogRoute.php
+++ app/Http/Middleware/LogRoute.php
@@ -18,18 +18,16 @@ class LogRoute
     {
         $response = $next($request);
 
-        if (app()->environment('local')) {
-            $log = [
-                'URI' => $request->getUri(),
-                'METHOD' => $request->getMethod(),
-                'REQUEST_BODY' => $request->all(),
-                'RESPONSE' => $response->getContent()
-            ];
-            if ($response->getStatusCode() == 200 || $response->getStatusCode() == 201 || $response->getStatusCode() == 202)
-                Log::info($request->getUri(), json_encode($log));
-            else
-                Log::error($request->getUri(), json_encode($log));
-        }
+        $log = [
+            'URI' => $request->getUri(),
+            'METHOD' => $request->getMethod(),
+            'REQUEST_BODY' => $request->all(),
+            'RESPONSE' => $response->getContent()
+        ];
+        if ($response->getStatusCode() == 200 || $response->getStatusCode() == 201 || $response->getStatusCode() == 202)
+            Log::info($request->getUri(), json_encode($log));
+        else
+            Log::error($request->getUri(), json_encode($log));
 
         return $response;
     }

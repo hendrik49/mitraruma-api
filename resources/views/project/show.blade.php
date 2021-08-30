aeb6594e2061c57be383cac8781fb812c1df9b4d
Hendrik <drikdoank@gmail.com>
2021-08-30 08:07:38 +0700

    fix missing column

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -47,7 +47,7 @@
                                         <div class="col-sm-6">
                                             <label for="title">No. Room</label>
                                             <input type="text" class="form-control" name="room_id"
-                                                placeholder="Masukkan no room" value="{{ $project->room_id }}" readonly>
+                                                placeholder="Masukkan no room" value="{{ $project->room_number }}" readonly>
                                         </div>
                                     </div>
                                     <div class="form-group row">

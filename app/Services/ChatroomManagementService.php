dd1ea949674d1ed2d6f40f7ce92ad458fcff2ad2
Hendrik <drikdoank@gmail.com>
2021-08-31 16:30:18 +0700

    fix order status error

--- app/Services/ChatroomManagementService.php
+++ app/Services/ChatroomManagementService.php
@@ -134,7 +134,7 @@ class ChatroomManagementService
         $params['consultation_id'] = $consultation['id'];
         $params['status'] = 'Pre-Purchase';
         $params['image_url'] = $user['user_picture_url'] ?? "";
-        $params['name'] = $user['display_name'] . '-AV-' . $consultation['order_number'];
+        $params['name'] = $consultation['name'] . '-AV-' . $consultation['order_number'];
         $params['text'] = 'Halo bisa mengerjakan projek ini?';
         $params['consultation_description'] = $consultation['description'];
         $params['room_type'] = 'admin-vendor';

a562671d5f9ffa07a4f0d5d3e657917039eb9892
Hendrik <drikdoank@gmail.com>
2021-09-07 19:20:10 +0700

    fix wrong naming

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -18,18 +18,56 @@
                         <table class="table table-bordered table-striped" id="projectrange">
                             <thead>
                                 <tr>
-                                    <th>No.</th>
-                                    <th>Order Number</th>
-                                    <th>Room Number</th>
-                                    <th>Customer Name</th>
-                                    <th>Customer Contact</th>
-                                    <th>Applicator Name</th>
-                                    <th>Tipe Proyek</th>
-                                    <th>Status</th>
-                                    <th>Date</th>
-                                    <th>Room ID</th>
-                                    <th>Consultation ID</th>
-                                    <th>Description</th>
+                                    <th>no.</th>
+                                    <th>order number</th>
+                                    <th>room number</th>
+                                    <th>customer name</th>
+                                    <th>customer contact</th>
+                                    <th>applicator name</th>
+                                    <th>tipe proyek</th>
+                                    <th>status</th>
+                                    <th>date</th>
+                                    {{-- <th>room ID</th>
+                                    <th>consultation ID</th> --}}
+                                    <th>description</th>
+
+                                    <th> amount spk customer </th>
+                                    <th> amount spk vendor </th>
+                                    <th> amount spk vendor net </th>
+                                    <th> mitraruma discount </th>
+                                    <th> applicator discount </th>
+                                    <th> commision </th>
+                                    <th> other expanse </th>
+                                    <th> total expanse </th>
+                                    <th> expanse note </th>
+                                    <th> project note </th>
+                                    <th> material buy </th>
+                                    <th> booking fee </th>
+                                    <th> term payment vendor </th>
+                                    <th> term payment customer </th>
+                                
+                                    <th> termin customer 1 </th>
+                                    <th> termin customer 2 </th>
+                                    <th> termin customer 3 </th>
+                                    <th> termin customer 4 </th>
+                                    <th> termin customer 5 </th>
+                                    <th> termin customer 1 date </th>
+                                    <th> termin customer 2 date </th>
+                                    <th> termin customer 3 date </th>
+                                    <th> termin customer 4 date </th>
+                                    <th> termin customer 5 date </th>
+                            
+                                    <th> termin vendor 1 </th>
+                                    <th> termin vendor 2 </th>
+                                    <th> termin vendor 3 </th>
+                                    <th> termin vendor 4 </th>
+                                    <th> termin vendor 5 </th>
+                                    <th> termin vendor 1 date </th>
+                                    <th> termin vendor 2 date </th>
+                                    <th> termin vendor 3 date </th>
+                                    <th> termin vendor 4 date </th>
+                                    <th> termin vendor 5 date </th>
+                                    <th> payment retention date </th>                                    
                                     <th>Actions</th>
                                 </tr>
                             </thead>
@@ -45,9 +83,47 @@
                                         <td width="10%">{{ $project->service_type }}</td>
                                         <td>{{ $project->status }}</td>
                                         <td>{{ $project->created_at->format('Y-m-d') }}</td>
-                                        <td>{{ $project->room_id }}</td>
-                                        <td>{{ $project->consultation_id }}</td>
+                                        {{-- <td>{{ $project->room_id }}</td>
+                                        <td>{{ $project->consultation_id }}</td> --}}
                                         <td>{{ $project->description }}</td>
+
+                                        <td>{{ $project->amount_spk_customer}}</td>
+                                        <td>{{ $project->amount_spk_vendor}}</td>
+                                        <td>{{ $project->amount_spk_vendor_net}}</td>
+                                        <td>{{ $project->mitraruma_discount}}</td>
+                                        <td>{{ $project->applicator_discount}}</td>
+                                        <td>{{ $project->commision}}</td>
+                                        <td>{{ $project->other_expanse}}</td>
+                                        <td>{{ $project->total_expanse}}</td>
+                                        <td>{{ $project->expanse_note}}</td>
+                                        <td>{{ $project->project_note}}</td>
+                                        <td>{{ $project->material_buy}}</td>
+                                        <td>{{ $project->booking_fee}}</td>
+                                        <td>{{ $project->term_payment_vendor}}</td>
+                                        <td>{{ $project->term_payment_customer}}</td>
+                                    
+                                        <td>{{ $project->termin_customer_1}}</td>
+                                        <td>{{ $project->termin_customer_2}}</td>
+                                        <td>{{ $project->termin_customer_3}}</td>
+                                        <td>{{ $project->termin_customer_4}}</td>
+                                        <td>{{ $project->termin_customer_5}}</td>
+                                        <td>{{ $project->termin_customer_1_date}}</td>
+                                        <td>{{ $project->termin_customer_2_date}}</td>
+                                        <td>{{ $project->termin_customer_3_date}}</td>
+                                        <td>{{ $project->termin_customer_4_date}}</td>
+                                        <td>{{ $project->termin_customer_5_date}}</td>
+                                
+                                        <td>{{ $project->termin_vendor_1}}</td>
+                                        <td>{{ $project->termin_vendor_2}}</td>
+                                        <td>{{ $project->termin_vendor_3}}</td>
+                                        <td>{{ $project->termin_vendor_4}}</td>
+                                        <td>{{ $project->termin_vendor_5}}</td>
+                                        <td>{{ $project->termin_vendor_1_date}}</td>
+                                        <td>{{ $project->termin_vendor_2_date}}</td>
+                                        <td>{{ $project->termin_vendor_3_date}}</td>
+                                        <td>{{ $project->termin_vendor_4_date}}</td>
+                                        <td>{{ $project->termin_vendor_5_date}}</td>
+                                        <td>{{ $project->payment_retention_date}}</td>
                                         <td width="15%">
                                             <a href="{{ route('proyek.edit', ['proyek' => $project->id]) }}"
                                                 class="btn btn-sm btn-primary"> <i class="glyphicon glyphicon-edit"></i>
@@ -82,9 +158,9 @@
                 'dom': 'Bfrtip',
                 'buttons': [{
                     extend: 'excel',
-                    exportOptions: {
-                        columns: [0, 1, 2, 3, 4, 5]
-                    }
+                    // exportOptions: {
+                    //     columns: [0, 1, 2, 3, 4, 5]
+                    // }
                 }, ]
             });
         });

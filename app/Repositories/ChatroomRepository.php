43931d2df40713a5c35da776e9a071c7294b3c00
Hendrik <drikdoank@gmail.com>
2021-09-14 21:05:27 +0700

    money remove

--- app/Repositories/ChatroomRepository.php
+++ app/Repositories/ChatroomRepository.php
@@ -11,11 +11,14 @@ class ChatroomRepository
         \App\Services\FirebaseService $firebase
     ) {
         $this->model = $firebase->database('chatroom');
+        $this->modelAccounts = $firebase->database('accounts');
+        $this->firebase = $firebase;
     }
 
-    public function findById($id){
+    public function findById($id)
+    {
         $model = $this->model->document($id)->snapshot();
-        if($model->data()){
+        if ($model->data()) {
             $chatroom = $model->data();
             $chatroom['id'] = $model->id();
             return $chatroom;
@@ -23,7 +26,8 @@ class ChatroomRepository
         return null;
     }
 
-    public function find($params){
+    public function find($params)
+    {
 
         $model = $this->model;
         $model = $this->filterBuilder($model, $params);
@@ -39,44 +43,81 @@ class ChatroomRepository
         return $chatrooms;
     }
 
-    public function create($params) {
+    public function create($params)
+    {
         $model = $this->model->newDocument();
         $model->set($params);
+
+        if ($params['roomType'] == "admin-customer") {
+            $modelUser = $this->modelAccounts->document($params['userId']);
+            $room = $modelUser->collection('chatroom')->newDocument();
+            $roomNew = array();
+            $roomNew['roomId'] = $params['roomId'];
+            $room->set($roomNew);
+
+            $modelAdmin = $this->modelAccounts->document($params['adminId']);
+            $roomAdmin = $modelAdmin->collection('chatroom')->newDocument();
+            $roomNew = array();
+            $roomNew['roomId'] = $params['roomId'];
+            $roomAdmin->set($roomNew);
+        }else if ($params['roomType'] == "admin-vendor") {
+            $modelUser = $this->modelAccounts->document($params['applicatorId']);
+            $room = $modelUser->collection('chatroom')->newDocument();
+            $roomNew = array();
+            $roomNew['roomId'] = $params['roomId'];
+            $room->set($roomNew);
+
+            $modelAdmin = $this->modelAccounts->document($params['adminId']);
+            $roomAdmin = $modelAdmin->collection('chatroom')->newDocument();
+            $roomNew = array();
+            $roomNew['roomId'] = $params['roomId'];
+            $roomAdmin->set($roomNew);
+        }else{
+
+            $modelUser = $this->modelAccounts->document($params['userId']);
+            $room = $modelUser->collection('chatroom')->newDocument();
+            $roomNew = array();
+            $roomNew['roomId'] = $params['roomId'];
+            $room->set($roomNew);
+
+            $modelV = $this->modelAccounts->document($params['applicatorId']);
+            $roomV = $modelV->collection('chatroom')->newDocument();
+            $roomNew = array();
+            $roomNew['roomId'] = $params['roomId'];
+            $roomV->set($roomNew);
+
+            $modelAdmin = $this->modelAccounts->document($params['adminId']);
+            $roomAdmin = $modelAdmin->collection('chatroom')->newDocument();
+            $roomNew = array();
+            $roomNew['roomId'] = $params['roomId'];
+            $roomAdmin->set($roomNew);
+        }
+
         return $this->findById($model->id());
     }
 
-    public function update($params, $id){
-        $this->model->document($id)->set($params);
-        return $this->findById($id);
-    }
+    private function filterBuilder($model, $params)
+    {
 
-    public function deleteById($id) {
-        return $this->model->document($id)->delete();
-    }
+        $model = $model->orderBy('date', 'desc');
 
-    private function filterBuilder($model, $params) {
-
-        $model = $model->orderBy('date' , 'desc');
-
-        if(isset($params['user_id'])) {
+        if (isset($params['user_id'])) {
             $model = $model->where('userId', '=', $params['user_id']);
         }
-        if(isset($params['vendor_user_id'])) {
+        if (isset($params['vendor_user_id'])) {
             $model = $model->where('applicatorId', '=', $params['vendor_user_id']);
         }
-        if(isset($params['consultation_id'])) {
+        if (isset($params['consultation_id'])) {
             $model = $model->where('consultationId', '=', $params['consultation_id']);
         }
-        if(isset($params['room_type'])) {
+        if (isset($params['room_type'])) {
             $model = $model->where('roomType', '=', $params['room_type']);
         }
         $model = $model->limit($params['limit'] ?? 10);
-        if(isset($params['start_after'])) {
+        if (isset($params['start_after'])) {
             $model = $model->startAfter([$params['start_after'] ?? '']);
         }
 
         return $model;
-
     }
-
 }
--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -11,8 +11,32 @@ class FirebaseService
         $this->database = app('firebase.firestore')->database();
     }
 
-    public function database($model) {
+    public function database($model)
+    {
         return $this->database->collection($model);
     }
 
+    public  function setChatroomIdtoUser($data)
+    {
+        return $this->database
+            ->collection("accounts")
+            ->document($data->roomId)            
+            ->collection("chatroom");
+    }
+
+    public function setChatroomIdtoAdmin($data)
+    {
+        return $this->database
+            ->collection("accounts")
+            ->document($data->roomId)            
+            ->collection("chatroom");
+    }
+
+    public function setChatroomIdtoApplicator($data)
+    {
+        return $this->database
+            ->collection("accounts")
+            ->document($data->roomId)
+            ->collection("chatroom");
+    }
 }

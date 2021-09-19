9a8494a9822f02fe758761fb2717de70097f32b6
Hendrik <drikdoank@gmail.com>
2021-09-19 11:34:52 +0700

    update

--- app/Helpers/OrderStatus.php
+++ app/Helpers/OrderStatus.php
@@ -23,78 +23,91 @@ class OrderStatus
                 "activity": "Start of Conversation",
                 "action": "Start of Conversation",
                 "phase": "Pre-Purchase",
+                "type":"general",
                 "by": ""
               },
               "120": {
                 "activity": "Detail Project Uploaded",
                 "action": "Detail Project Uploaded",
                 "phase": "Pre-Purchase",
+                "type":"general",
                 "by": "customer"
               },
               "130": {
                 "activity": "Selected Applicator join the conversation",
                 "action": "Selected Applicator join the conversation",
                 "phase": "Pre-Purchase",
+                "type":"general",
                 "by": ""
               },
               "140": {
                 "activity": "Quotation Uploaded",
                 "action": "Upload Quotation",
                 "phase": "Pre-Purchase",
+                "type":"document",
                 "by": "vendor"
               },
               "160": {
                 "activity": "Booking Fee Paid",
                 "action": "Pay Booking Fee",
                 "phase": "Pre-Purchase",
+                "type":"payment",
                 "by": "customer"
               },
               "210": {
                 "activity": "Build of Quantity (BOQ) Uploaded",
                 "action": "Upload Build of Quantity (BOQ)",
                 "phase": "Design Phase",
+                "type":"document",
                 "by": "vendor"
               },
               "220": {
                 "activity": "Revision of Build of Quantity (BOQ) Uploaded",
                 "action": "Upload Revision Build of Quantity (BOQ)",
                 "phase": "Design Phase",
+                "type":"document",
                 "by": "vendor"
               },
               "230": {
                 "activity": "Final Revision of Build of Quantity (BOQ) Uploaded",
                 "action": "Upload Final Revision Build of Quantity (BOQ)",
                 "phase": "Design Phase",
+                "type":"document",
                 "by": "vendor"
               },
               "310": {
                 "activity": "Draft Contract Uploaded",
                 "action": "Upload Draft Contract",
                 "phase": "Construction Phase",
+                "type":"document",
                 "by": "admin"
               },
               "320": {
                 "activity": "Signed Contract Uploaded by Customer",
                 "action": "Upload Signed Contract",
                 "phase": "Construction Phase",
+                "type":"document",
                 "by": "customer"
               },
               "321": {
                 "activity": "Signed Contract Uploaded by Applicator",
                 "action": "Upload Signed Contract",
                 "phase": "Construction Phase",
+                "type":"document",
                 "by": "vendor"
               },
               "330": {
                 "activity": "First Payment Paid by Customer",
                 "action": "Pay First Payment",
                 "phase": "Construction Phase",
+                "type":"payment",
                 "by": "customer"
               },
               "331": {
                 "activity": "First Payment Paid by Admin",
                 "action": "Pay First Payment",
                 "phase": "Construction Phase",
+                "type":"payment",
                 "by": "admin",
                 "hidden": "customer"
               },
@@ -102,24 +115,28 @@ class OrderStatus
                 "activity": "Project Has Started",
                 "action": "Upload Progress Checklist",
                 "phase": "Project Started",
+                "type":"general",
                 "by": "vendor"
               },
               "420": {
                 "activity": "Project 50% WIP",
                 "action": "Upload Progress 50%",
                 "phase": "Project Started",
+                "type":"document",
                 "by": "vendor"
               },
               "430": {
                 "activity": "Second Payment Paid by Customer",
                 "action": "Pay Second Payment",
                 "phase": "Project Started",
+                "type":"payment",
                 "by": "customer"
               },
               "431": {
                 "activity": "Second Payment Paid by Admin",
                 "action": "Pay Second Payment",
                 "phase": "Project Started",
+                "type":"payment",
                 "by": "admin",
                 "hidden": "customer"
               },
@@ -127,11 +144,13 @@ class OrderStatus
                 "activity": "Acceptance to Continue The Project",
                 "action": "Accept & Continue Project",
                 "phase": "Project Started",
+                "type":"general",
                 "by": "customer"
               },
               "450": {
                 "activity": "Project 70% WIP",
                 "action": "Upload Progress 70%",
+                "type":"document",
                 "phase": "Project Started",
                 "by": "vendor"
               },
@@ -139,12 +158,14 @@ class OrderStatus
                 "activity": "Last Payment Paid by Customer",
                 "action": "Pay Last Payment",
                 "phase": "Project Started",
+                "type":"payment",
                 "by": "customer"
               },
               "470": {
                 "activity": "Third Payment Paid by Admin",
                 "action": "Pay Third Payment",
                 "phase": "Project Started",
+                "type":"payment",
                 "by": "admin",
                 "hidden": "customer"
               },
@@ -152,18 +173,21 @@ class OrderStatus
                 "activity": "Acceptance to Continue The Project",
                 "action": "Accept & Continue Project",
                 "phase": "Project Started",
+                "type":"general",
                 "by": "customer"
               },
               "510": {
                 "activity": "Project 100% Finished",
                 "action": "Upload Progress 100%",
                 "phase": "Project Ended",
+                "type":"general",
                 "by": "vendor"
               },
               "520": {
                 "activity": "Acceptance and Check List to Ended The Project",
                 "action": "Accept & End The Project",
                 "phase": "Project Ended",
+                "type":"general",
                 "by": "customer"
               },
               "530": {
@@ -171,12 +195,14 @@ class OrderStatus
                 "action": "Pay Last Payment",
                 "phase": "Project Ended",
                 "by": "admin",
+                "type":"payment",
                 "hidden": "customer"
               },
               "540": {
                 "activity": "Feedback and Ratings",
                 "action": "Give Feedback and Ratings",
                 "phase": "Project Ended",
+                "type":"general",
                 "by": "customer"
               }
             }';

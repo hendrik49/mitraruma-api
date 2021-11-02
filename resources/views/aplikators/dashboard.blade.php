2b13576be7aab4574e4c1f2395687c6f3940c0b3
Hendrik <drikdoank@gmail.com>
2021-11-02 09:07:58 +0700

    add masters

--- resources/views/aplikators/dashboard.blade.php
+++ resources/views/aplikators/dashboard.blade.php
@@ -1,119 +1,32 @@
+@php
+// charting
+$pieStatusLabel = $pieStatus->pluck('label');
+$pieStatusValue = $pieStatus->pluck('value');
+
+$pieLabel = $pie->pluck('label');
+$pieValue = $pie->pluck('value');
+
+$stackBarData = [];
+foreach ($pie as $p) {
+    $el = [];
+    $el['label'] = $p->label;
+    $el['data'] = [0, $p->value];
+    $el['backgroundColor'] = sprintf('#%06X', mt_rand(0, 0xffffff));
+    $stackBarData[] = $el;
+}
+if ($pf) {
+    $per = json_encode([$pf->quality ?? 0, $pf->responsiveness_to_customer ?? 0, $pf->responsiveness_to_mitraruma ?? 0, $pf->behaviour ?? 0, $pf->helpful ?? 0, $pf->commitment ?? 0, $pf->activeness ?? 0]);
+    $overall = round(($pf->quality + $pf->responsiveness_to_customer + $pf->responsiveness_to_mitraruma + $pf->behaviour + $pf->helpful + $pf->commitment + $pf->activeness) / 7);
+} else {
+    $per = json_encode([0, 0, 0, 0, 0, 0, 0]);
+    $overall = 0;
+}
+@endphp
+
 @extends('layouts.app')
 @section('title') Overview Data @endsection
 @section('content')
     <section class="content">
-        <!-- Small boxes (Stat box) -->
-        {{-- @if (Auth::user()->user_type == 'admin')
-            <div class="row">
-                <div class="col-md-3 mt-4">
-                    <div class="card ">
-                        <div class="card-header ui-sortable-handle">
-                            <h3 class="card-title">
-                                <i class="fas fa-chart-pie mr-1"></i>
-                                Filter
-                            </h3>
-                        </div>
-                        <div class="card-body">
-                            <div class="form-group{{ $errors->has('service-type') ? ' has-error' : '' }}">
-                                <label for="service-type">Service Type</label>
-                                <select class="form-control" id="service-type-field" name="skill_set">
-                                    <option value="service">Home Service</option>
-                                    <option value="renovation">Home Renovation</option>
-                                </select>
-                                @if ($errors->has('service-type'))
-                                    <span class="help-block">{{ $errors->first('service-type') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group{{ $errors->has('coverage-area') ? ' has-error' : '' }}">
-                                <label for="coverage-area">Service Area</label>
-                                <select class="form-control select2" id="coverage-area-field" name="skill_set">
-                                    @foreach ($masters->where('name', 'area-coverage')->first()->value as $p)
-                                        @foreach ($p['child'] as $v)
-                                            <option value="{{ $v }}">{{ $v }}</option>
-                                        @endforeach
-                                    @endforeach
-                                </select>
-                                @if ($errors->has('coverage-area'))
-                                    <span class="help-block">{{ $errors->first('coverage-area') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group @if ($errors->has('skill_set')) has-error @endif">
-                                <label for="triwulan-field">Skill Set</label>
-                                <select class="form-control" id="skill_set-field" name="skill_set">
-                                    @foreach ($masters->where('name', 'skill-set')->first()->value as $p)
-                                        <option value="{{ $p['code'] }}">{{ $p['name'] }}</option>
-                                    @endforeach
-                                </select>
-                                @if ($errors->has('skill_set'))
-                                    <span class="help-block">{{ $errors->first('skill_set') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group{{ $errors->has('customer_segmentation') ? ' has-error' : '' }}">
-                                <label for="customer_segmentation">Segmentation</label>
-                                <select class="form-control" id="coverage-area-field" name="skill_set">
-                                    @foreach ($masters->where('name', 'segment')->first()->value as $p)
-                                        <option value="{{ $p['code'] }}">{{ $p['name'] }}</option>
-                                    @endforeach
-                                </select>
-                                @if ($errors->has('customer_segmentation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('customer_segmentation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="well well-sm mb-2">
-                                <button type="submit" value="filter" class="btn btn-primary pull-right right"> Filter
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-9 mt-4">
-                    <div class="card ">
-                        <div class="card-header ui-sortable-handle">
-                            <h3 class="card-title">
-                                <i class="fas fa-table mr-1"></i>
-                                Data
-                            </h3>
-                        </div>
-                        <div class="card-body">
-                            <table class="table dt-dashboard" id="userrange">
-                                <thead>
-                                    <tr>
-                                        <th>User ID</th>
-                                        <th>Name</th>
-                                        <th>Capacity</th>
-                                        <th>On Going</th>
-                                        <th>% Capacity</th>
-                                        <th>Overall Score</th>
-                                        <th>Compitency</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($aplikators as $key => $review)
-                                        <tr>
-                                            <td>{{ $review->ID }}</td>
-                                            <td>{{ $review->display_name }}</td>
-                                            <td>{{ $review->capacity }} </td>
-                                            <td>{{ $review->projects->where('status', '<>', 'project ended')->count() }}
-                                            </td>
-                                            <td>{{ ($review->projects->where('status', '<>', 'project ended')->count() / $review->capacity) * 100 }}
-                                            </td>
-                                            <td>{{ round($review->review['overall_score'], 2) ?? '' }} </td>
-                                            <td>{{ $review->review['quality'] ?? '' }}
-                                                {{ $review->review['responsiveness_to_customer'] ?? '' }}
-                                                {{ $review->review['responsiveness_to_mitraruma'] ?? '' }}
-                                                {{ $review->review['helpful'] ?? '' }}
-                                                {{ $review->review['behaviour'] ?? '' }} </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endif --}}
         <div class="row mt-2">
             <!-- ./col -->
             <div class="col-lg-6 col-xs-8">
@@ -129,11 +42,19 @@
                             <div class="col-lg-6 col-xs-6">
                                 <div class="col-xs-5">
                                     <div class="form-group @if ($errors->has('user_picture_url')) has-error @endif">
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
+                                        @if ($overal)
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                        @else
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                        @endif
                                         <div class="img">
                                             <img class="img img-responsive" style="margin-bottom:5px;" width="100"
                                                 height="75" id="avatar" @if ($user->user_picture_url) src="{{ $user->user_picture_url }}" @else src="http://nanoup.net/assets/userdata/avatar/thumbs/default-avatar.png" @endif>
@@ -571,30 +492,7 @@
 
         </div>
     @endsection
-    @php
-        // charting
-        $pieStatusLabel = $pieStatus->pluck('label');
-        $pieStatusValue = $pieStatus->pluck('value');
-        
-        $pieLabel = $pie->pluck('label');
-        $pieValue = $pie->pluck('value');
-        
-        $stackBarData = [];
-        foreach ($pie as $p) {
-            $el = [];
-            $el['label'] = $p->label;
-            $el['data'] = [0, $p->value];
-            $el['backgroundColor'] = sprintf('#%06X', mt_rand(0, 0xffffff));
-            $stackBarData[] = $el;
-        }
-        if ($pf) {
-            $per = json_encode([$pf->quality ?? 0, $pf->responsiveness_to_customer ?? 0, $pf->responsiveness_to_mitraruma ?? 0, $pf->behaviour ?? 0, $pf->helpful ?? 0, $pf->commitment ?? 0, $pf->activeness ?? 0]);
-            $overall = round(($pf->quality + $pf->responsiveness_to_customer + $pf->responsiveness_to_mitraruma + $pf->behaviour + $pf->helpful + $pf->commitment + $pf->activeness) / 7);
-        } else {
-            $per = json_encode([0, 0, 0, 0, 0, 0, 0]);
-            $overall = 0;
-        }
-    @endphp
+
     @section('scripts')
         @parent
         <script type="text/javascript">
